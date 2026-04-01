e1e19811c3efa6582679236061526d9b4b55a951
farrles <bogdan.abder@yandex.ru>
2026-04-01 14:43:03 +0500

    Реализация

--- Surgery.h
+++ Surgery.h
@@ -1,5 +1,23 @@
 #pragma once
-class Surgery
-{
-};
 
+#include "Treatment.h"
+
+using namespace std;
+
+class Surgery : public Treatment 
+{
+private:
+    string surgeryType;
+    string riskLevel;
+
+public:
+    Surgery(
+        const string& description, 
+        const string& date, 
+        double cost, 
+        const string& surgeryType, 
+        const string& riskLevel
+    );
+
+    void printInfo() const override;
+};
