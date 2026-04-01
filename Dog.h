bfda7d6160021aaa90af27540bac53de3a031448
farrles <bogdan.abder@yandex.ru>
2026-04-01 14:31:51 +0500

    Починка выводов и чистка кода + читабельность

--- Dog.h
+++ Dog.h
@@ -1,31 +1,38 @@
 #pragma once
+
 #include <iostream>
 #include "Animal.h"
+
 using namespace std;
-//#ifndef DOG_H
-//#define DOG_H
+
 class Dog : public Animal 
 {
 private:
     bool isVaccinated;
 
 public:
-    Dog(int id, const string& name, const string& breed, int age, const string& admissionDate, bool isVaccinated = false);
+    Dog(
+        int id, 
+        const string& name, 
+        const string& breed, 
+        int age, 
+        const string& admissionDate, 
+        bool isVaccinated = false
+    );
 
-    string getFeedSchedule() const override; // расписание кормления
-    string getTreatmentProtocol() const override; // протокол лечения
+    string getFeedSchedule() const override;
+
+    string getTreatmentProtocol() const override;
+
     void printInfo() const override;
 
-    // Методы для собак
     void vaccinate() 
     { 
         isVaccinated = true; 
     }
+
     bool getIsVaccinated() const 
     { 
         return isVaccinated; 
     }
 };
-
-//#endif
-
