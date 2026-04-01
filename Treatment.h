d782bbd2dfc98e6ea3ea1e2b368e4e6372098fca
farrles <bogdan.abder@yandex.ru>
2026-04-01 14:41:30 +0500

    Реализация

--- Treatment.h
+++ Treatment.h
@@ -1,5 +1,42 @@
 #pragma once
-class Treatment
-{
-};
 
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Treatment 
+{
+protected:
+    string description;
+    string date;
+    double cost;
+
+public:
+    Treatment(
+        const string& description, 
+        const string& date, 
+        double cost
+    );
+
+    virtual ~Treatment() 
+    {
+    }
+
+    virtual void printInfo() const = 0;
+
+    string getDescription() const 
+    { 
+        return description; 
+    }
+
+    string getDate() const 
+    { 
+        return date; 
+    }
+
+    double getCost() const 
+    { 
+        return cost; 
+    }
+};
