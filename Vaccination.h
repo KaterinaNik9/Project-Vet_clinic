181c28efce70944c707e3fa23b47273661ed81ec
farrles <bogdan.abder@yandex.ru>
2026-04-01 14:42:21 +0500

    Реализицая

--- Vaccination.h
+++ Vaccination.h
@@ -1,5 +1,23 @@
 #pragma once
-class Vaccination
-{
-};
 
+#include "Treatment.h"
+
+using namespace std;
+
+class Vaccination : public Treatment 
+{
+private:
+    string vaccineName;
+    string expirationDate;
+
+public:
+    Vaccination(
+        const string& description, 
+        const string& date, 
+        double cost, 
+        const string& vaccineName, 
+        const string& expirationDate
+    );
+
+    void printInfo() const override;
+};
