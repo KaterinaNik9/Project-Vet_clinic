a7166d30f44f17e3079c6ae36c57ebb33a076aa5
farrles <bogdan.abder@yandex.ru>
2026-04-01 14:42:43 +0500

    Реализация

--- Vaccination.cpp
+++ Vaccination.cpp
@@ -1,1 +1,30 @@
 #include "Vaccination.h"
+#include <iostream>
+
+using namespace std;
+
+Vaccination::Vaccination(
+    const string& description, 
+    const string& date, 
+    double cost, 
+    const string& vaccineName, 
+    const string& expirationDate
+)
+    : Treatment(
+        description, 
+        date, 
+        cost
+    ), 
+      vaccineName(vaccineName), 
+      expirationDate(expirationDate) 
+{
+}
+
+void Vaccination::printInfo() const 
+{
+    cout << "Процедура: Вакцинация" << endl;
+    cout << "Описание: " << description << endl;
+    cout << "Препарат: " << vaccineName << endl;
+    cout << "Дата: " << date << endl;
+    cout << "Стоимость: " << cost << endl;
+}
