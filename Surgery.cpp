68b970f009b8ad49e84b182a8746773676d3f355
farrles <bogdan.abder@yandex.ru>
2026-04-01 14:43:27 +0500

    Реализация

--- Surgery.cpp
+++ Surgery.cpp
@@ -1,1 +1,30 @@
 #include "Surgery.h"
+#include <iostream>
+
+using namespace std;
+
+Surgery::Surgery(
+    const string& description, 
+    const string& date, 
+    double cost, 
+    const string& surgeryType, 
+    const string& riskLevel
+)
+    : Treatment(
+        description, 
+        date, 
+        cost
+    ), 
+      surgeryType(surgeryType), 
+      riskLevel(riskLevel) 
+{
+}
+
+void Surgery::printInfo() const 
+{
+    cout << "Процедура: Хирургическое вмешательство" << endl;
+    cout << "Тип операции: " << surgeryType << endl;
+    cout << "Уровень риска: " << riskLevel << endl;
+    cout << "Дата: " << date << endl;
+    cout << "Стоимость: " << cost << endl;
+}
