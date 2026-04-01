e1254dfcd4ac30070211d827af3351d108027d79
farrles <bogdan.abder@yandex.ru>
2026-04-01 14:44:36 +0500

    Реализация

--- Appointment.cpp
+++ Appointment.cpp
@@ -1,1 +1,34 @@
 #include "Appointment.h"
+#include <iostream>
+
+using namespace std;
+
+Appointment::Appointment(
+    shared_ptr<Animal> animal, 
+    const string& vetName, 
+    const string& date, 
+    const string& time, 
+    const string& reason
+)
+    : animal(animal), 
+      vetName(vetName), 
+      date(date), 
+      time(time), 
+      reason(reason) 
+{
+}
+
+void Appointment::printAppointmentInfo() const 
+{
+    cout << "--- Запись на прием ---" << endl;
+    cout << "Дата и время: " << date << " | " << time << endl;
+    cout << "Врач: " << vetName << endl;
+    cout << "Причина: " << reason << endl;
+    
+    if (animal) 
+    {
+        cout << "Пациент: " << animal->getName() << " (" << animal->getSpecies() << ")" << endl;
+    }
+    
+    cout << "-----------------------" << endl;
+}
