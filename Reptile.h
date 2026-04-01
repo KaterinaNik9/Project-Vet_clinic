0ef6bb5486738bf7a5c39d23e3caa17563c01026
farrles <bogdan.abder@yandex.ru>
2026-04-01 14:36:39 +0500

    Починка выводов и чистка кода + читабельность

--- Reptile.h
+++ Reptile.h
@@ -2,31 +2,38 @@
 
 #include <iostream>
 #include "Animal.h"
+
 using namespace std;
 
 class Reptile : public Animal 
 {
 private:
-    bool isColdBlooded; // холоднокровное животное 
-    string habitatType; // тип (где живет)
+    bool isColdBlooded;
+    string habitatType;
 
 public:
-    Reptile(int id, const string& name, const string& breed, int age, const string& admissionDate, const string& habitatType = "Террариум");
+    Reptile(
+        int id, 
+        const string& name, 
+        const string& breed, 
+        int age, 
+        const string& admissionDate, 
+        const string& habitatType = "РўРµСЂСЂР°СЂРёСѓРј"
+    );
 
-    string getFeedSchedule() const override;// расписание кормления
-    string getTreatmentProtocol() const override;// протокол лечения
+    string getFeedSchedule() const override;
+
+    string getTreatmentProtocol() const override;
+
     void printInfo() const override;
 
-    // Методы для рептилий
     void setHabitatType(const string& habitat) 
     { 
         habitatType = habitat; 
     }
+
     string getHabitatType() const 
     { 
         return habitatType; 
     }
 };
-
-
-
