800520615e6c488f0a6343a1973eaa44cd6be12a
farrles <bogdan.abder@yandex.ru>
2026-04-07 20:14:14 +0500

    Fix: Починка принта

--- Creatures/Animal.cpp
+++ Creatures/Animal.cpp
@@ -1,52 +1,29 @@
 #include "Animal.h"
 #include <iostream>
-#include <vector>
-#include <string>
 
 using namespace std;
 
-Animal::Animal(
-    int id, 
-    const string& name, 
-    const string& species, 
-    const string& breed, 
-    int age, 
-    const string& admissionDate
-) 
-    : id            (id)
-    , name          (name)
-    , species       (species)
-    , breed         (breed)
-    , age           (age)
-    , admissionDate (admissionDate)
-    , isActive      (true)
-{
-}
+Animal::Animal(int id, const string& name, const string& species, const string& breed, int age, const string& admissionDate)
+    : id(id), name(name), species(species), breed(breed), age(age), admissionDate(admissionDate), isActive(true) {}
 
-void Animal::addMedicalRecord(const MedicalRecord& record) 
-{
+void Animal::addMedicalRecord(const MedicalRecord& record) {
     medicalHistory.push_back(record);
 }
 
-void Animal::printMedicalHistory() const 
-{
-    if (medicalHistory.empty()) 
-    {
+void Animal::printMedicalHistory() const {
+    if (medicalHistory.empty()) {
         cout << "Медицинские записи для " << name << " не найдены." << endl;
         return;
     }
 
     cout << "Медицинская история для " << name << " (ID: " << id << "):" << endl;
-
-    for (const auto& record : medicalHistory) 
-    {
-        record.display(); 
+    for (const auto& record : medicalHistory) {
+        record.print();
         cout << endl;
     }
 }
 
-void Animal::printInfo() const 
-{
+void Animal::printInfo() const {
     cout << "ID: " << id << endl;
     cout << "Имя: " << name << endl;
     cout << "Вид: " << species << endl;
