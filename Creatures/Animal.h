63de9684ce5ff724b7103a009138f75e21eb7205
farrles <bogdan.abder@yandex.ru>
2026-04-07 20:13:32 +0500

    Fix: Починка вектора и памяти

--- Creatures/Animal.h
+++ Creatures/Animal.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <memory>
+#include "MedicalRecord.h"
+
 using namespace std;
-class MedicalRecord;
-class Animal 
+
+class Animal
 {
 protected:
     int id;
@@ -12,88 +15,33 @@ protected:
     string breed;
     int age;
     string admissionDate;
-    vector<MedicalRecord> medicalHistory;
+    vector<MedicalRecord> medicalHistory; 
     bool isActive;
 
 public:
     Animal(int id, const string& name, const string& species, const string& breed, int age, const string& admissionDate);
-    
-    virtual ~Animal() 
-    {
-    }
+    virtual ~Animal() {}
 
-    int getId() const 
-    { 
-        return id; 
-    }
+    int getId() const { return id; }
+    string getName() const { return name; }
+    string getSpecies() const { return species; }
+    string getBreed() const { return breed; }
+    int getAge() const { return age; }
+    string getAdmissionDate() const { return admissionDate; }
+    bool getIsActive() const { return isActive; }
+    const vector<MedicalRecord>& getMedicalHistory() const { return medicalHistory; }
 
-    string getName() const 
-    { 
-        return name; 
-    }
-
-    string getSpecies() const 
-    { 
-        return species; 
-    }
-
-    string getBreed() const 
-    { 
-        return breed; 
-    }
-
-    int getAge() const 
-    { 
-        return age; 
-    }
-
-    string getAdmissionDate() const 
-    { 
-        return admissionDate; 
-    }
-
-    bool getIsActive() const 
-    { 
-        return isActive; 
-    }
-
-    const vector<MedicalRecord>& getMedicalHistory() const 
-    { 
-        return medicalHistory; 
-    }
-
-    void setName(const string& _name) 
-    { 
-        name = _name; 
-    }
-
-    void setBreed(const string& _breed) 
-    { 
-        breed = _breed; 
-    }
-
-    void setAge(int _age) 
-    { 
-        age = _age; 
-    }
-
-    void setIsActive(bool active) 
-    { 
-        isActive = active; 
-    }
+    void setName(const string& _name) { name = _name; }
+    void setBreed(const string& _breed) { breed = _breed; }
+    void setAge(int _age) { age = _age; }
+    void setIsActive(bool active) { isActive = active; }
 
     void addMedicalRecord(const MedicalRecord& record);
-    
     void printMedicalHistory() const;
 
     virtual string getFeedSchedule() const = 0;
-    
     virtual string getTreatmentProtocol() const = 0;
-
     virtual void printInfo() const;
 
-    bool operator==(const Animal& other) const 
-    { 
-        return id == other.id; 
-    }
+    bool operator==(const Animal& other) const { return id == other.id; }
 };
